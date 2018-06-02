f2120d16d48ffa486a5e8557f419c35d7d5e1524
Ibrahim <ibinshikh@hotmail.com>
2018-06-02 15:40:50 +0300

    Updated list item

--- html/ListItem.php
+++ html/ListItem.php
@@ -28,10 +28,65 @@
  * A class that represents List Item node.
  *
  * @author Ibrahim
- * @version 1.0
+ * @version 1.1
  */
 class ListItem extends HTMLNode{
-    public function __construct() {
+    /**
+     * A boolean value that is set to <b>TRUE</b> in case the list item only 
+     * accepts text.
+     * @var boolean
+     * @since 1.1 
+     */
+    private $textOnly;
+    /**
+     * Constructs new list item
+     * @param boolean $textOnly [Optional] Set to <b>TRUE</b> to make the list item 
+     * accepts text only. Default is <b>FALSE</b>.
+     * @param string $text [Optional] The text that will be displayed by the list 
+     * item. Ignored if the parameter <b>$textOnly</b> is set to <b>FALSE</b>.
+     * @since 1.0
+     */
+    public function __construct($textOnly=false,$text='') {
         parent::__construct('li', TRUE);
+        $this->textOnly = $textOnly === TRUE ? TRUE : FALSE;
+        $this->setText($text);
+    }
+    /**
+     * Sets the text to display on the list item.
+     * @param string $text The text to display. Only set if the node 
+     * accepts text.
+     * @since 1.1
+     */
+    public function setText($text) {
+        if($this->isTextOnly()){
+            if($this->childNodes()->get(0) != NULL){
+                $this->childNodes()->get(0)->setText($text);
+            }
+            else{
+                $textNode = new HTMLNode('', '', TRUE);
+                $textNode->setText($text);
+                parent::addChild($textNode);
+            }
+        }
+    }
+    /**
+     * Checks if the node only accepts text or not.
+     * @return boolean <b>TRUE</b> is returned if the node accepts text 
+     * only.
+     */
+    public function isTextOnly() {
+        return $this->textOnly;
+    }
+    /**
+     * Adds new child node to the list item.
+     * @param HTMLNode $node The node that will be added. Node that the 
+     * function will work only if the list item allows things other 
+     * than the text in its body.
+     * @since 1.1
+     */
+    public function addChild($node) {
+        if(!$this->isTextOnly()){
+            parent::addChild($node);
+        }
     }
 }
