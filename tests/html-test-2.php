4803c7ddce2eed5834ce6e8fc0cb2c4ea9a69ce9
Ibrahim <ibinshikh@hotmail.com>
2018-06-06 09:50:04 +0300

    Updated test

--- tests/html-test-2.php
+++ tests/html-test-2.php
@@ -26,15 +26,15 @@
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
-require '../Node.php';
-require '../LinkedList.php';
-require '../Stack.php';
+require '../structs/Node.php';
+require '../structs/LinkedList.php';
+require '../structs/Stack.php';
 require '../html/HTMLNode.php';
 require '../html/HTMLDoc.php';
 require '../html/HeadNode.php';
-$node = new HTMLNode('html');
-$node->setAttribute('lang','en');
-$head = new HeadNode();
-$head->setTitle('Hello');
-$node->addChild($head);
-echo $node;
+
+
+$doc = new HTMLDoc();
+$titleNode = $doc->getChildrenByTag('title')->get(0);
+$doc->getHeadNode()->removeNode($titleNode);
+echo $doc->getChildrenByTag('html')->get(0)->asCode(TRUE);
