5e74d7dfafbb6c7c3c53e52e6c57558a6830838c
Ibrahim <ibinshikh@hotmail.com>
2018-07-24 09:13:21 +0300

    Fixed required files location

--- tests/StackTest.php
+++ tests/StackTest.php
@@ -26,8 +26,8 @@
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
-require '../Node.php';
-require '../Stack.php';
+require '../structs/Node.php';
+require '../structs/Stack.php';
 class StackTest{
     private $stack;
     public function __construct($max=0) {
