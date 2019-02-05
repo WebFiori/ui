6fc770e1be04245808a3b1c287ea02d11d93ea55
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-02-05 07:33:28 +0300

    Create CodeSnippet.php

---
+++ html/CodeSnippet.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace phpStructs\html;
+
+/**
+ * Description of CodeSnippet
+ *
+ * @author Ibrahim
+ */
+class CodeSnippet extends HTMLNode{
+    //put your code here
+}
+/**
+ * <div class="code-snippet">
+                                            <p>Code</p>
+                                            <div class="code-display">
+                                                <div class="lines-numbers">
+                                                    <span class="line-number">1</span>
+                                                    <span class="line-number">2</span>
+                                                    <span class="line-number">3</span>
+                                                </div>
+                                                <pre><code><br>&lt;link rel="stylesheet" href="<a href="https://www.programmingacademia.com/res/css/code-theme.css" target="_blank">https://www.programmingacademia.com/res/css/code-theme.css</a>"&gt;</code></pre>
+                                            </div>
+                                        </div>
+ */
