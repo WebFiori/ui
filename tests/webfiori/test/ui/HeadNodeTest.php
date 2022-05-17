688da7d84cdd38f8c8dbace2ebd2a0cda043914d
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-05-18 01:49:43 +0300

    Update HeadNodeTest.php

--- tests/webfiori/test/ui/HeadNodeTest.php
+++ tests/webfiori/test/ui/HeadNodeTest.php
@@ -789,4 +789,17 @@ class HeadNodeTest extends TestCase {
         $this->assertEquals(1,$node->childrenCount());
         $this->assertEquals('',$node->getPageTitle());
     }
+    /**
+     * @test
+     */
+    public function testSetTitle01() {
+        $node = new HeadNode('Hello');
+        $this->assertEquals(2,$node->childrenCount());
+        $this->assertNotNull($node->getTitleNode());
+        $this->assertEquals('Hello',$node->getPageTitle());
+        $node->setTitle('');
+        $this->assertEquals(1,$node->childrenCount());
+        $this->assertEquals('',$node->getPageTitle());
+        $this->assertNotNull($node->getTitleNode());
+    }
 }
